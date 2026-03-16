7360baf2970888327d8fa3e90b5d06370752a199
richard <richardsunday0812@gmail.com>
2026-03-16 14:29:48 +0100

    Enhance Home page layout with new sections and styles; add fixed header for better navigation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,33 @@
+import "./Home.css";
+import chefImage from "../assets/cheff.png";
+
 export function HomePage() {
   return (
     <>
-      <h1>Home Page</h1>
+      <main>
+        <section className="display">
+          <div className="ad-message">
+            <h1>Quick and Delicious Meals</h1>
+            <p>
+              Browse our menu, pick your favourite meals, and enjoy fast
+              delivery right to your doorstep!
+            </p>
+            <div className="order-btn">
+              <button>Sign up Now</button>
+            </div>
+          </div>
+          <div className="ad-image">
+            <img src={chefImage} alt="Chef" />
+          </div>
+        </section>
+        <section className="main">
+          <div className="welcome">
+            <h2>Our Menu</h2>
+            <p>Enjoy the best food in town!</p>
+          </div>
+          <div className="meal-container"></div>
+        </section>
+      </main>
     </>
   );
 }
