eef3eae00b936c91c5ec106f0b7d14258a77a25c
richard <richardsunday0812@gmail.com>
2026-03-16 16:00:43 +0100

    Refactor code structure for improved readability and maintainability

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,23 +1,104 @@
+import meal2 from "../images/image2.png";
+
 export function Meals() {
   return (
     <div className="meals-container">
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
+
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
+
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
+
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
+
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
+
       <div className="meal-card">
-        <div className="meal-image"></div>
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
+      </div>
+
+      <div className="meal-card">
+        <div className="meal-image">
+          <img src={meal2} alt="" />
+        </div>
+        <h3 className="title">Meal 1</h3>
+        <p className="description">
+          A delicious meal made with fresh ingredients and a blend of spices to
+        </p>
+        <p className="price">$12.99</p>
+        <div className="cart-btn-container">
+          <button className="order-btn">Add to cart</button>
+        </div>
       </div>
     </div>
   );
